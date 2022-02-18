bc492179823c59767cd56114798e3939e5948b57
Evelinn <evelinn.idenfors@gmail.com>
2022-02-18 21:21:44 +0100

    fixed it

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
 window.onload = () => {
+  const NEXT_BTN_ID = "next-btn";
+  const PREV_BTN_ID = "previous-btn";
+  const PROGRESS_ID = "progress";
+  const CIRCLE_CLASS = "circle";
+  const ACTIVE_CIRCLE_CLASS = "active"
+
   const updateProgressWidth = (active, total) => {
     const progressPercentage = (active - 1) / (total - 1) * 100;
-    console.log((active - 1) / (total - 1) * 100);
-    document.getElementById("progress").style.width = `${progressPercentage}%`;
 
-    console.log(document.getElementById("progress").style.width);
+    document.getElementById(PROGRESS_ID).style.width = `${progressPercentage}%`;
   };
 
-  document.getElementById("next-btn").onclick = () => {
-    let activeCircles = document.getElementsByClassName("circle active");
-    let circles = document.getElementsByClassName("circle");
+  document.getElementById(NEXT_BTN_ID).onclick = () => {
+    let activeCircles = document.getElementsByClassName(`${CIRCLE_CLASS} ${ACTIVE_CIRCLE_CLASS}`);
+    let circles = document.getElementsByClassName(CIRCLE_CLASS);
 
-    if (activeCircles.length === circles.length  - 1) {
-      document.getElementById("next-btn").disabled = true;
+    const isLastElement = activeCircles.length === circles.length  - 1;
+    if (isLastElement) {
+      document.getElementById(NEXT_BTN_ID).disabled = true;
     }
-    if (activeCircles.length < circles.length) {
-      circles[activeCircles.length].classList.add("active");
-      document.getElementById("previous-btn").disabled = false;
+    const shouldActivateNextCircle = activeCircles.length < circles.length;
+    if (shouldActivateNextCircle) {
+      circles[activeCircles.length].classList.add(ACTIVE_CIRCLE_CLASS);
+      document.getElementById(PREV_BTN_ID).disabled = false;
     }
     updateProgressWidth(activeCircles.length, circles.length);
   };
 
-  document.getElementById("previous-btn").onclick = () => {
-    let activeCircles = document.getElementsByClassName("circle active");
-    let circles = document.getElementsByClassName("circle");
+  document.getElementById(PREV_BTN_ID).onclick = () => {
+    let activeCircles = document.getElementsByClassName(`${CIRCLE_CLASS} ${ACTIVE_CIRCLE_CLASS}`);
+    let circles = document.getElementsByClassName(CIRCLE_CLASS);
 
-    if (activeCircles.length > 1) {
-      circles[activeCircles.length - 1].classList.remove("active");
-      document.getElementById("next-btn").disabled = false;
+    const isNotInitialState = activeCircles.length > 1;
+
+    if (isNotInitialState) {
+      circles[activeCircles.length - 1].classList.remove(ACTIVE_CIRCLE_CLASS);
+      document.getElementById(NEXT_BTN_ID).disabled = false;
     }
-    if (activeCircles.length === 1) {
-      document.getElementById("previous-btn").disabled = true;
+    const isLowestActiveCircle = activeCircles.length === 1;
+    if (isLowestActiveCircle) {
+      document.getElementById(PREV_BTN_ID).disabled = true;
     }
     updateProgressWidth(activeCircles.length, circles.length);
   };
